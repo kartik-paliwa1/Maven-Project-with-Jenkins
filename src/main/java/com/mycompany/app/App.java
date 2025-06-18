522129e6b90e1ce6f05e7344ad65809f35d3c0de
Kartik-Paliwa1 <kartikp752003@gmail.com>
2025-06-18 11:51:09 +0530

    Initial commit of simple Java Maven app

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,19 @@
+// src/main/java/com/mycompany/app/App.java
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from Maven!" );
+    }
+
+    public String getGreeting() {
+        return "Hello World!";
+    }
+}
+
---
+++ src/main/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,25 @@
+// src/test/java/com/mycompany/app/AppTest.java
+package com.mycompany.app;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testAppGreeting()
+    {
+        App app = new App();
+        assertEquals("Hello World!", app.getGreeting());
+    }
+}
+
+
